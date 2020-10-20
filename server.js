1b82102bc14981fb4ee60e153ed08b818be9e163
Juliette Gondon <juliettegondon@gmail.com>
2020-10-20 18:24:43 -0400

    heroku setup

--- server.js
+++ server.js
@@ -1,36 +1,42 @@
-//////// dependencies
-const { notStrictEqual } = require('assert');
-var express = require('express');
-var path = require('path');
-var fs = require('fs');
+//////// dependencies & variables
+const { notStrictEqual } = require("assert");
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
 
 var app = express();
-var PORT = 3000;
+var PORT = 8080;
 
 const notes = require("./Develop/db/db.json");
 console.log(Array.isArray(notes));
 
+//middleware makes data available before it hits route
+//setup to handle URL encodings and json that goes between function calls
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
 
 
 //////// routes
-// GET requests to the 2 given html files 
+
+// GET requests to the 2 given html files & API
 // GET reads db.json and returns saved notes as JSON
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
 });
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
 });
-// GET -  API route to get notes
-app.get('/api/notes', (req, res) => {
+// GET -  API route to get notes /// ASK TA
+app.get("/api/notes", (req, res) => {
     res.json(notes);
 });
 
 
-// POST request to API ROUTE, get user input on frontend, add to db.json, and append to frontend.
+// POST request to API ROUTE,get user input on frontend, add to db.json, and append to frontend.
 // JSON function returns as string
-app.post('/api/notes', (req, res) => {
+app.post("/api/notes", (req, res) => {
        var addNote = req.body; 
        notes.push(addNote);
        fs.writeFileSync(path.join(__dirname, './Develop/db/db.json'),JSON.stringify(notes))
