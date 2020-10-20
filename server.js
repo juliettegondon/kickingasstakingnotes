8bceba7785bc3b6b26d698b8bed55b37a14c9f05
Juliette Gondon <juliettegondon@gmail.com>
2020-10-20 14:28:58 -0400

    starting get and post requests

--- server.js
+++ server.js
@@ -1,21 +1,44 @@
-var express = require("express");
+//////// dependencies
+const { notStrictEqual } = require('assert');
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+
 var app = express();
 var PORT = 3000;
 
-const db = require("./db/db.json");
-console.log(Array.isArray(db));
+const notes = require("./Develop/db/db.json");
+console.log(Array.isArray(notes));
 
-// set variables for express and PORT
 
-// create API routes 
-// GET - reads db.json and returns saved notes as JSON
 
+//////// routes
+// GET requests to the 2 given html files 
+// GET reads db.json and returns saved notes as JSON
 app.get('/', (req, res) => {
-    res.send('hello world')
-})
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+});
+// GET -  API route to get notes
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+
+// POST request to API ROUTE, get user input on frontend, add to db.json, and append to frontend.
+// JSON function returns as string
+app.post('/api/notes', (req, res) => {
+       var addNote = req.body; 
+       notes.push(addNote);
+       fs.writeFileSync(path.join(__dirname, './Develop/db/db.json'),JSON.stringify(notes))
+});
+
+//// listener
 app.listen(PORT, () => {
     console.log(`listening at http://localhost:${PORT}`)
-})
-// POST - get user input on frontend, add to db.json, and append to frontend.
+});
+
 // DELETE - enter id of note to delete, delete note, write notes. 
